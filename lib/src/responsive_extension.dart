1a1ed31a7f9da5b3c5e0cb1e9531c760801d92cf
VANN <vanthey@blue.com.kh>
2024-07-11 11:04:23 +0700

    add feature

--- lib/src/responsive_extension.dart
+++ lib/src/responsive_extension.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/cupertino.dart';
 
-final double physicalHeight = WidgetsBinding
-    .instance.platformDispatcher.views.first.physicalSize.height;
+/// Get the device screen height
+final double physicalHeight =
+    WidgetsBinding.instance.platformDispatcher.views.first.physicalSize.height;
+
+/// Get the device screen width
 final double physicalWidth =
     WidgetsBinding.instance.platformDispatcher.views.first.physicalSize.width;
+
+/// Get the device pixel
 final double devicePixelRatio =
     WidgetsBinding.instance.platformDispatcher.views.first.devicePixelRatio;
 
+/// Calculate the size of given value that can scale on screen using baseWidth
+/// than convert it to screen pixel
 double _calculateSize(double size, [int baseWidth = 350]) {
   final screenWidth = physicalWidth / devicePixelRatio;
   final screenHeight = physicalHeight / devicePixelRatio;
@@ -15,10 +22,12 @@ double _calculateSize(double size, [int baseWidth = 350]) {
   return (size + (originalScale - size) * 0.5);
 }
 
+/// Get value from int and scale it base on screen size
 extension ScaleSizeExtension on int {
   double get scale => _calculateSize(toDouble());
 }
 
+/// Get value from int and scale it base on screen size
 extension ScaleSizeDoubleExtension on double {
   double get scale => _calculateSize(toDouble());
 }
